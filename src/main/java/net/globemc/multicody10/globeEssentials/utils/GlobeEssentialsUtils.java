23abb76e6f588345c9d8d32b0672be44ded828e2
VRILGQD <67159692+VRILGQD@users.noreply.github.com>
2024-11-09 14:21:02 -0500

    Delete src/main/java/net/globemc/multicody10/globeEssentials directory

--- src/main/java/net/globemc/multicody10/globeEssentials/utils/GlobeEssentialsUtils.java
+++
@@ -1,8 +0,0 @@
-package net.globemc.multicody10.globeEssentials.utils;
-
-import org.bukkit.ChatColor;
-
-public class GlobeEssentialsUtils {
-    public static final String mapPrefix = ChatColor.AQUA + "[Map]" + ChatColor.DARK_GRAY + " » ";
-    public static final String voidDeathPrefix = ChatColor.AQUA + "[VoidDeath]" + ChatColor.DARK_GRAY + " » ";
-}
