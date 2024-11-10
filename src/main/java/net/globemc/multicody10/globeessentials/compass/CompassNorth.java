8680e730cb428b5a2d7abb0daafb038eb2db08c0
VRILGQD <67159692+VRILGQD@users.noreply.github.com>
2024-11-10 11:56:38 -0500

    Add files via upload

--- src/main/java/net/globemc/multicody10/globeessentials/compass/CompassNorth.java
+++ src/main/java/net/globemc/multicody10/globeessentials/compass/CompassNorth.java
@@ -1,19 +1,41 @@
 package net.globemc.multicody10.globeessentials.compass;
 
-import io.papermc.paper.threadedregions.scheduler.EntityScheduler;
 import net.globemc.multicody10.globeessentials.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CompassMeta;
 
 public class CompassNorth implements Listener {
     public CompassNorth(Main plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (player.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
-                EntityScheduler scheduler = player.getScheduler();
-                scheduler.runAtFixedRate(plugin, _ -> player.setCompassTarget(player.getLocation().add(0.0D, 0.0D, -100.0D)), null, 0L, 30L);
+    }
+    @EventHandler
+    public void JoinCompassPoint(PlayerMoveEvent e) {
+        Player player = e.getPlayer();
+        ItemStack compass = new ItemStack(Material.COMPASS);
+        CompassMeta compassMeta = (CompassMeta) compass.getItemMeta();
+        if (player.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
+            if (!compassMeta.hasLodestone()) {
+                player.setCompassTarget(player.getLocation().add(0.0D, 0.0D, -1000.0D));
+            }
+        }
+    }
+    @EventHandler
+    public void TeleportCompassPoint(PlayerTeleportEvent e) {
+        Player player = e.getPlayer();
+        ItemStack compass = new ItemStack(Material.COMPASS);
+        CompassMeta compassMeta = (CompassMeta) compass.getItemMeta();
+        if (player.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
+            if (!compassMeta.hasLodestone()) {
+                player.setCompassTarget(player.getLocation().add(0.0D, 0.0D, -1000.0D));
             }
         }
     }
