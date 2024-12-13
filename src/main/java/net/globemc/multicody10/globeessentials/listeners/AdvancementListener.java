e932153f81f006ed6e96ae861fcbdf950c7116b9
VRILGQD <67159692+VRILGQD@users.noreply.github.com>
2024-12-13 15:59:57 -0500

    Update AdvancementListener.java

--- src/main/java/net/globemc/multicody10/globeessentials/listeners/AdvancementListener.java
+++ src/main/java/net/globemc/multicody10/globeessentials/listeners/AdvancementListener.java
@@ -1,12 +1,12 @@
 package net.globemc.multicody10.globeessentials.listeners;
 
 import com.palmergames.bukkit.towny.event.TownAddResidentEvent;
+import io.papermc.paper.threadedregions.scheduler.GlobalRegionScheduler;
 import net.globemc.multicody10.globeessentials.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 
 public class AdvancementListener implements Listener {
     public AdvancementListener(Main plugin) {
@@ -15,8 +15,9 @@ public class AdvancementListener implements Listener {
     @EventHandler
     public void UponTownJoin(TownAddResidentEvent event) {
         Player player = event.getResident().getPlayer();
+        GlobalRegionScheduler scheduler = Bukkit.getGlobalRegionScheduler();
         if (player != null) {
-            Bukkit.getGlobalRegionScheduler().execute((Plugin) this, () -> {
+            scheduler.execute(Main.getPlugin(Main.class), () -> {
                 String command = "minecraft:advancement grant " + player.getName() + " only globemc:world/make_town";
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
             });
